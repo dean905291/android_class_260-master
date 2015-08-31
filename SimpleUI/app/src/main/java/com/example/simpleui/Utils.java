1aad21b1c0e6a57d337cfefc9c00a2edd8eb2ccc
dean905291 <dean905291@gmail.com>
2015-08-31 20:39:16 +0800

    wirte_and_read_file_to_show_on_the_textView

---
+++ SimpleUI/app/src/main/java/com/example/simpleui/Utils.java
@@ -0,0 +1,47 @@
+package com.example.simpleui;
+
+import android.content.Context;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Created by DeanH on 8/31/15.
+ */
+public class Utils {
+
+    public static void  writeFile(Context context, String filename, String text) {
+        try {
+            FileOutputStream fos = context.openFileOutput(filename, Context.MODE_APPEND);
+            fos.write(text.getBytes());
+            fos.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+
+    }
+    public static String readFile(Context context, String filename){
+
+        try {
+            FileInputStream fis = context.openFileInput(filename);
+            byte[] buffer = new byte[1024];
+            fis.read(buffer);
+
+            return new String(buffer);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+
+}
